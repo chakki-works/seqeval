f7e0d1da782b55ec4a3d426d5418e737a032fc51
Hironsan <light.tree.1.13@gmail.com>
2018-02-15 15:40:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ required = ['numpy>=1.14.0']
 
 setup(
     name=NAME,
-    version='0.0.1',
+    version='0.0.2',
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
