77e3d636793885cfa725a4bf2c776e818d789816
Hironsan <light.tree.1.13@gmail.com>
2018-02-16 13:30:04 +0900

    Add statistical test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,6 +3,8 @@ Evaluation test is performed for the following dataset.
 https://www.clips.uantwerpen.be/conll2000/chunking/output.html
 """
 import os
+import random
+import subprocess
 import unittest
 
 from seqeval.metrics import f1_score, accuracy_score, classification_report, precision_score, recall_score
@@ -15,9 +17,9 @@ class TestMetrics(unittest.TestCase):
     def setUpClass(cls):
         cls.y_true = ['O', 'O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'O', 'B-PER', 'I-PER']
         cls.y_pred = ['O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'I-MISC', 'O', 'B-PER', 'I-PER']
-        dir = os.path.join(os.path.dirname(__file__), 'data')
-        true_file = os.path.join(dir, 'true.txt')
-        pred_file = os.path.join(dir, 'pred.txt')
+        cls.dir = os.path.join(os.path.dirname(__file__), 'data')
+        true_file = os.path.join(cls.dir, 'true.txt')
+        pred_file = os.path.join(cls.dir, 'pred.txt')
         cls.y_true = cls.load_data(true_file)
         cls.y_pred = cls.load_data(pred_file)
         cls.y_true_nested = cls.load_nested_data(true_file)
@@ -49,7 +51,7 @@ class TestMetrics(unittest.TestCase):
 
     def test_get_entities(self):
         y_true = ['O', 'O', 'O', 'B-MISC', 'I-MISC', 'I-MISC', 'O', 'B-PER', 'I-PER']
-        print(get_entities(y_true))
+        self.assertEqual(get_entities(y_true), [('MISC', 3, 5), ('PER', 7, 8)])
 
     def test_f1_score(self):
         score = f1_score(self.y_true, self.y_pred)
@@ -78,3 +80,72 @@ class TestMetrics(unittest.TestCase):
     def test_classification_report(self):
         print(classification_report(self.y_true, self.y_pred))
         print(classification_report(self.y_true_nested, self.y_pred_nested))
+
+    def test_compare_score(self):
+        filepath = 'eval_data.txt'
+        for i in range(10000):
+            print('Iteration: {}'.format(i))
+            y_true, y_pred = self.generate_eval_data(filepath)
+            with open(filepath) as f:
+                output = subprocess.check_output(['perl', 'conlleval.pl'], stdin=f).decode('utf-8')
+                acc_true, p_true, r_true, f1_true = self.parse_conlleval_output(output)
+
+                acc_pred = accuracy_score(y_true, y_pred)
+                p_pred = precision_score(y_true, y_pred)
+                r_pred = recall_score(y_true, y_pred)
+                f1_pred = f1_score(y_true, y_pred)
+
+                self.assertLess(abs(acc_pred - acc_true), 1e-4)
+                self.assertLess(abs(p_pred - p_true), 1e-4)
+                self.assertLess(abs(r_pred - r_true), 1e-4)
+                self.assertLess(abs(f1_pred - f1_true), 1e-4)
+
+        os.remove(filepath)
+
+    def parse_conlleval_output(self, text):
+        eval_line = text.split('\n')[1]
+        items = eval_line.split(' ')
+        accuracy, precision, recall = [item[:-2] for item in items if '%' in item]
+        f1 = items[-1]
+        accuracy = float(accuracy) / 100
+        precision = float(precision) / 100
+        recall = float(recall) / 100
+        f1 = float(f1) / 100
+
+        return accuracy, precision, recall, f1
+
+    def generate_eval_data(self, filepath):
+        types = ['PER', 'MISC', 'ORG', 'LOC']
+        prefixes = ['B', 'I', 'O']
+        report = ''
+        raw_fmt = '{} {} {} {}\n'
+        y_true, y_pred = [], []
+        tmp_true, tmp_pred = [], []
+        for i in range(1000):
+            type_true = random.choice(types)
+            type_pred = random.choice(types)
+            prefix_true = random.choice(prefixes)
+            prefix_pred = random.choice(prefixes)
+            true_out = 'O' if prefix_true == 'O' else '{}-{}'.format(prefix_true, type_true)
+            pred_out = 'O' if prefix_pred == 'O' else '{}-{}'.format(prefix_pred, type_pred)
+            report += raw_fmt.format('X', 'X', true_out, pred_out)
+            tmp_true.append(true_out)
+            tmp_pred.append(pred_out)
+
+            # end of sentence
+            if random.random() > 0.95:
+                report += '\n'
+                y_true.append(tmp_true)
+                y_pred.append(tmp_pred)
+                tmp_true = []
+                tmp_pred = []
+        else:
+            report += '\n'
+            y_true.append(tmp_true)
+            y_pred.append(tmp_pred)
+
+        with open(filepath, 'w') as f:
+            f.write(report)
+
+        return y_true, y_pred
+
