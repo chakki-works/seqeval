b2ee970fe7bc7c13eedf1c85a90e020b621e37cb
Hironsan <light.tree.1.13@gmail.com>
2020-10-08 10:09:23 +0900

    Add two reporters

---
+++ tests/test_reporters.py
@@ -0,0 +1,26 @@
+import pytest
+from seqeval.reporters import DictReporter, StringReporter
+
+
+@pytest.mark.parametrize(
+    'rows, expected',
+    [
+        ([], {}),
+        (
+            [['PERSON', 0.82, 0.79, 0.81, 24]],
+            {
+                'PERSON': {
+                    'precision': 0.82,
+                    'recall': 0.79,
+                    'f1-score': 0.81,
+                    'support': 24
+                }
+            }
+        )
+    ]
+)
+def test_dict_reporter_output(rows, expected):
+    reporter = DictReporter()
+    for row in rows:
+        reporter.write(*row)
+    assert reporter.report() == expected
